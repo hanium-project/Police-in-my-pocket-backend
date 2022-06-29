ee5fe4393cd0949a3b7eb52df711f4149e32a33b
seeunjang <wisjang1@naver.com>
2022-06-29 11:26:48 +0900

    Rename: 패키지 구조 수정

--- src/main/java/com/pocket/police/domain/user/entity/Account.java
+++ src/main/java/com/pocket/police/domain/user/entity/Account.java
@@ -1,5 +1,6 @@
 package com.pocket.police.domain.user.entity;
 
+import com.pocket.police.global.Timestamped;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,8 @@ import javax.persistence.*;
 @NoArgsConstructor   //기본 생성자 자동 추가
 @Entity    //테이블과 링크될 클래스임을 나타냄
 @Table(name = "account")
-public class Account {
+public class Account extends Timestamped {
     @Id   //해당 테이블의 PK 필드를 나타냄
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)   -> 자동으로 생성할 필요가 없기 때문에 사용하지 않음
     private String user_id;
 
     @Column(name = "password", nullable = false)  //해당 테이블의 칼럼을 나타냄
@@ -30,21 +30,35 @@ public class Account {
     @Column(name = "phone_number", nullable = false)
     private String phone_number;
 
-    @Column(name = "gender", nullable = false)
-    private String gender;
-
     @Column(name = "user_siren_code", nullable = true)
     private int user_siren_code;
 
-    @Builder  //해당 클래스의 빌더 패턴 클래스를 생성
-    public Account(String user_id, String password, String user_name, java.sql.Date birth, String address, String phone_number, String gender, int user_siren_code) {
+    @Builder  //해당 클래스의 빌더 패턴 클래스를 생성polin
+    public Account(String user_id, String password, String user_name, java.sql.Date birth, String address, String phone_number, int user_siren_code) {
         this.user_id = user_id;
         this.password = password;
         this.user_name = user_name;
         this.birth = birth;
         this.address = address;
         this.phone_number = phone_number;
-        this.gender = gender;
         this.user_siren_code = user_siren_code;
     }
+
+    public void update(String user_id, String password, String user_name, java.sql.Date birth, String address, String phone_number, int user_siren_code) {
+        this.user_id = user_id;
+        this.password = password;
+        this.user_name = user_name;
+        this.birth = birth;
+        this.address = address;
+        this.phone_number = phone_number;
+        this.user_siren_code = user_siren_code;
+    }
+
+    public boolean login(String id,String pw){
+        if(user_id.equals(id) && password.equals(pw))
+            return true;
+        else
+            return false;
+    }
+
 }
