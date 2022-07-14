b3e88c45e1150b70c050e0387c93713f174c8b9a
hee <jkgt0521@naver.com>
2022-07-15 01:55:01 +0900

    Feat: #9 로그인 jwt 토큰

---
+++ src/main/java/com/pocket/police/global/security/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.pocket.police.global.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity)
+}
