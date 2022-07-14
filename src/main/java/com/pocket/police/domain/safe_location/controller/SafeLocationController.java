9d653e5c25339266a09f41caed3604c51fcf8a32
seeunjang <wisjang1@naver.com>
2022-07-14 20:34:24 +0900

    Rename: 치안 시설 jpa 수정 완

--- src/main/java/com/pocket/police/domain/safe_location/controller/SafeLocationController.java
+++ src/main/java/com/pocket/police/domain/safe_location/controller/SafeLocationController.java
@@ -1,16 +1,16 @@
 package com.pocket.police.domain.safe_location.controller;
 
 
+import com.pocket.police.domain.safe_location.dto.LocationRequestDto;
 import com.pocket.police.domain.safe_location.dto.LocationResponseDto;
 import com.pocket.police.domain.safe_location.entity.SafeLocation;
+import com.pocket.police.domain.safe_location.repository.SafeLocationRepository;
 import com.pocket.police.domain.safe_location.service.SafeLocationService;
 import com.pocket.police.domain.user.dto.AccountRequestDto;
+import com.pocket.police.domain.user.repository.AccountRepository;
 import com.pocket.police.domain.user.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +20,11 @@ import java.util.List;
 public class SafeLocationController {
 
     private final SafeLocationService safelocationservice;
+
+    @PostMapping("/v1/safe")
+    public Long save(@RequestBody final LocationRequestDto params) {
+        return safelocationservice.save(params);
+    }
 
     @GetMapping("/v1/safe")
     public List<SafeLocation> findAll() {
