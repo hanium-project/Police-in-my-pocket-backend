0e04fd33a7b2e03629c1f972d77316cf9cfea7ad
hee <jkgt0521@naver.com>
2022-07-25 19:00:28 +0900

    Feat: #9 jwt token login and auth

--- src/main/java/com/pocket/police/domain/user/service/AccountService.java
+++ src/main/java/com/pocket/police/domain/user/service/AccountService.java
@@ -32,7 +32,7 @@ public class AccountService {
         pw = passwordEncoder.encode(params.getPassword());
         params.setPassword(pw);
         Account entity = accountRepository.save(params.toEntity());
-        return entity.getUser_id();
+        return entity.getUserId();
     }
 
     @Transactional
@@ -47,21 +47,8 @@ public class AccountService {
     public String update(final String id, final AccountRequestDto params) {
 
         Account entity = accountRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
-        entity.update(params.getUser_id(), params.getPassword(), params.getUser_name(), params.getBirth(), params.getAddress(), params.getGender(),  params.getPhone_number(),
-                params.getUser_siren_code());
+        entity.update(params.getUserId(), params.getPassword(), params.getName(), params.getBirth(), params.getAddress(), params.getPhoneNumber(),
+                params.getUserSirenCode(), params.getGender());
         return id;
     }
-
-    @Transactional
-    public boolean login(final AccountRequestDto params) {
-        Account entity = accountRepository.findById(params.getUser_id()).orElseThrow(() -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
-        return entity.login(params.getUser_id(), params.getPassword());
-    }
-
-    @Transactional
-    public String findpw(final AccountRequestDto params){
-        Account entity = accountRepository.findById(params.getUser_id()).orElseThrow(() -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다."));
-        return entity.getPassword();
-    }
-
 }
--- src/main/java/com/pocket/police/global/security/WebSecurityConfig.java
+++ src/main/java/com/pocket/police/global/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -16,7 +15,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor  //final, @notNull이 붙은 필드의 생성자를 자동으로 생성
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
@@ -28,26 +29,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()   //http 기본 설정 해제
+                .cors().and()
                 .csrf().disable()   //csrf 보안 토큰 disable 처리
                 .formLogin().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //토큰 기반 인증이므로 세션 역시 사용하지 않음
                 .and()
                 .authorizeRequests()
-                // .antMatchers("/admin/**").hasRole("ADMIN")
-                //  .antMatchers("/api/v1/**").hasRole("USER")
-                //  .anyRequest().permitAll()
-                .antMatchers("/api/v1/**").permitAll()
-                .and();
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
-//                        UsernamePasswordAuthenticationFilter.class);
+                //.antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/api/v1/users/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                //.logout()
+                //.logoutRequestMatcher(new AntPathRequestMatcher("/api/v1/users/logout"))
+               // .logoutSuccessUrl("/api/v1/users/msg")
+               // .and()
+                //.requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 }
